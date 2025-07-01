c5ed0a63223d3bc5c5c6234a202e3bf191a0dc4d
Rushikesh <rushikeshnakhate@gmail.com>
2025-07-01 15:15:00 +0800

    Refactor gradle and fixed compilation issue .

--- java/plugins/quickfiximpl/src/main/java/com/applicationsx/quickfiximpl/QuickFixDemo.java
+++ java/plugins/quickfiximpl/src/main/java/com/applicationsx/quickfiximpl/QuickFixDemo.java
@@ -1,18 +1,17 @@
 package com.applicationsx.quickfiximpl;
 
 import quickfix.*;
-import quickfix.fix44.MessageCracker;
 
 public class QuickFixDemo {
-    
+
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Usage: QuickFixDemo [server|client]");
-            System.exit(1);
+            System.exit(100);
         }
-        
+
         String mode = args[0].toLowerCase();
-        
+
         try {
             if ("server".equals(mode)) {
                 runServer();
@@ -27,61 +26,59 @@ public class QuickFixDemo {
             e.printStackTrace();
         }
     }
-    
+
     private static void runServer() throws Exception {
         System.out.println("Starting QuickFix Server...");
-        
+
         // Load server configuration
         SessionSettings settings = new SessionSettings("src/main/resources/quickfix_server.cfg");
-        
+
         // Create application
         QuickFixServer application = new QuickFixServer();
-        
+
         // Create message store factory
         MessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        
+
         // Create log factory
         LogFactory logFactory = new FileLogFactory(settings);
-        
+
         // Create message factory
         MessageFactory messageFactory = new DefaultMessageFactory();
-        
+
         // Create acceptor
-        ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(
-            application, storeFactory, settings, logFactory, messageFactory);
-        
+        ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
+
         acceptor.start();
         System.out.println("Server started. Press Ctrl+C to stop.");
-        
+
         // Keep server running
         Thread.currentThread().join();
     }
-    
+
     private static void runClient() throws Exception {
         System.out.println("Starting QuickFix Client...");
-        
+
         // Load client configuration
         SessionSettings settings = new SessionSettings("src/main/resources/quickfix_client.cfg");
-        
+
         // Create application
         QuickFixClient application = new QuickFixClient();
-        
+
         // Create message store factory
         MessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        
+
         // Create log factory
         LogFactory logFactory = new FileLogFactory(settings);
-        
+
         // Create message factory
         MessageFactory messageFactory = new DefaultMessageFactory();
-        
+
         // Create initiator
-        SocketInitiator initiator = new SocketInitiator(
-            application, storeFactory, settings, logFactory, messageFactory);
-        
+        SocketInitiator initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
+
         initiator.start();
         System.out.println("Client started. Press Ctrl+C to stop.");
-        
+
         // Keep client running
         Thread.currentThread().join();
     }
